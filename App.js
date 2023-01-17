5d3a9cd725fa1afd96171b8faa39476e9f438bd5
SimonBisi <r0834462@student.thomasmore.be>
2023-01-17 14:09:32 +0100

    interface

--- App.js
+++ App.js
@@ -2,13 +2,47 @@ import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { useEffect, useState } from "react";
 import * as Location from "expo-location";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createTheme, ThemeProvider } from '@rneui/themed';
 import LocationComponent from "./components/location";
+
+const Tab = createBottomTabNavigator();
+const theme = createTheme({
+  lightColors: {
+    primary: '#1273de',
+  },
+  darkColors: {
+    primary: '#000',
+  },
+});
 
 export default function App() {
 
   return (
-    <View>
-      <LocationComponent/>
-    </View>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                switch (route.name) {
+                  case "Location":
+                    iconName = focused ? 'home-sharp' : 'home-outline';
+                    break;
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: '#1273de',
+              tabBarInactiveTintColor: 'gray',
+            })}
+          >
+            <Tab.Screen name="Location" component={LocationComponent} 
+              // options={{headerShown: false}}
+            />
+          </Tab.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
--- components/location.js
+++ components/location.js
@@ -1,9 +1,11 @@
-import { Button, StyleSheet, Text, View } from "react-native";
-import { useEffect, useState } from "react";
+import {Alert, Modal, StyleSheet, Text, Pressable, View} from 'react-native';
+import React, { useEffect, useState } from "react";
 import * as Location from "expo-location";
+
 
 function LocationComponent() {
   const [location, setLocation] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     Location.requestForegroundPermissionsAsync();
@@ -18,8 +20,30 @@ function LocationComponent() {
 
   return (
     <View style={styles.container}>
+      <Text style={{ fontSize: 30 }}>Raspberry Pi id: "1" {'\n'} </Text>
+        <Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Location set!</Text>
+            <Pressable style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable style={[styles.button, styles.buttonOpen]}
+        onPress={
+          async() => { await
+            getLocation()
+            setModalVisible(true)
+          }}>
+        <Text style={styles.textStyle}>Set location</Text>
+      </Pressable>
       <Text style={{ fontSize: 30 }}>{location}</Text>
-      <Button onPress={getLocation} title="Get" />
     </View>
   );
 }
@@ -32,5 +56,46 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     flexGrow: 1,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#2196F3',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
